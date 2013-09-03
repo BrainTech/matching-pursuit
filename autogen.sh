ead27edd85c8f8f600642dded669c5ce0ae693d4
Piotr Różański <ptr@mimuw.edu.pl>
2013-09-03 14:23:42 +0200

    Added information regarding MacOS static compilation

--- autogen.sh
+++ autogen.sh
@@ -17,4 +17,9 @@ if [ "x$1" != "x" ]; then
     exec ./configure "$@"
 else
     echo "run './configure && make' to compile"
+    echo
+    echo "INFORMATION: --enable-static does not work on MacOS."
+    echo "If you want to statically link FFTW library on MacOS, it is necessary"
+    echo "to specify the path to FFTW static library using --with-fftw3."
+    echo "No dynamic version of the FFTW library may exist in the same directory."
 fi
